512583b92668c264440a060c68546bae42f9f668
ZhangChunyang <zcyhun@gmail.com>
2019-01-07 07:02:20 +0800

    update planner.c

--- src/planner.c
+++ src/planner.c
@@ -28,3 +28,22 @@ uint8_t plan_next_block_index(uint8_t block_index){
 	}
 	return block_index;
 }
+
+// Returns the index of the previous block in the ring buffer
+static uint8_t plan_prev_block_index(uint8_t block_index){
+	if(block_index == 0) {
+		block_index = BLOCK_BUFFER_SIZE;
+	}
+	block_index--;
+	return block_index;
+}
+
+/*                 PLANNER SPEED DEFINITION
+
+                            +--------+  <- current->nominal_speed
+                           /          \
+ current->entry_speed ->  +            \
+                          |             + <- next->entry_speed (aka exit speed)
+						  +-------------+
+						      time -->
+*/
